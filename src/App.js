6ddb554ee66ef97e90af2cb3b36bed9ddc8fdf1c
eva nurr <eva.nurrohmah@koding.sch.id>
2019-12-26 13:57:28 +0700

    belajar react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Counter from './component/counter';
+import About from './page/about';
+import Home from './page/home';
+import Counterd from './component/counter-class';
+import Menu from './component/menu';
+import Data from './component/data-diri';
+import Makanan from './page/list-makanan';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route path="/" exact component ={Home} />
+        <Route path="/Counter" exact component ={Counter} />
+        <Route path="/about"exact component ={About} />
+        <Route path="/counterd"exact component ={Counterd} />
+        <Route path="/menu"exact component ={Menu} />
+        <Route path="/data"exact component ={Data} />
+        <Route path="/makanan"exact component ={Makanan} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
